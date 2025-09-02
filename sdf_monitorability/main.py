0fed50cbfd7d7278266d3ddc0a3fa6d55513054d
Matt MacDermott <mjm121@ic.ac.uk>
2025-09-02 13:46:48 -0700

    set up repo

---
+++ sdf_monitorability/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from sdf-monitorability!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ tests/dummy_test.py
@@ -0,0 +1,5 @@
+from sdf_monitorability.main import main
+
+
+def dummy_test():
+    assert not main()
